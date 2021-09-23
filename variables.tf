cad43c73c1e0340130a4ea0c925099432d2053a4
Pete Johnson <petecj2@gmail.com>
2021-09-23 15:39:32 -0400

    Defined valid variable values

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  #default = "us-east-2"
+  default = "us-east-2"
 }
 
 variable "ami" {
-  #default = "ami-0d5d9d301c853a04a"
+  default = "ami-0d5d9d301c853a04a"
 }
 
 variable "instance_type" {
-  #default = "t2.micro"
+  default = "t2.micro"
 }
